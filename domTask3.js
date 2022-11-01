c2c16ce7c49b4d8343952cf0e8eee86e29d86cc2
Rachit3819 <rachitagarwal2016@gmail.com>
2022-11-01 22:08:48 +0530

    domtask5

--- domTask3.js
+++ domTask3.js
@@ -28,12 +28,22 @@
 //items.style.color="green"
 
 
-var items=document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[3])
-items[2].style.backgroundColor="green"
-items[2].style.fontWeight="bold"
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold"
-}
+//var items=document.getElementsByClassName('list-group-item')
+//console.log(items)
+//console.log(items[3])
+//items[2].style.backgroundColor="green"
+//items[2].style.fontWeight="bold"
+//for(var i=0;i<items.length;i++){
+//    items[i].style.fontWeight="bold"
+//}
 
+
+
+var li=document.getElementsByTagName('li')
+console.log(li)
+console.log(li[3])
+li[2].style.backgroundColor="green"
+li[2].style.fontWeight="bold"
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight="bold"
+}
