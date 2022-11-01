1ce05767756dfef258848798c5dc2724ea8227ef
Rachit3819 <rachitagarwal2016@gmail.com>
2022-11-02 00:38:08 +0530

    DomTask6

--- domTask3.js
+++ domTask3.js
@@ -39,11 +39,43 @@
 
 
 
-var li=document.getElementsByTagName('li')
-console.log(li)
-console.log(li[3])
-li[2].style.backgroundColor="green"
-li[2].style.fontWeight="bold"
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight="bold"
+// var li=document.getElementsByTagName('li')
+// console.log(li)
+// console.log(li[3])
+// li[2].style.backgroundColor="green"
+// li[2].style.fontWeight="bold"
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight="bold"
+// }
+
+
+
+
+//QuerySlector
+// var header = document.querySelector ('#main-header')
+// header.style.borderBottom ='solid 3px #ccc'
+
+// var input = document.querySelector ('input')
+// input.value='HelloWorld'
+
+// var seconditem=document.querySelector(".list-group-item:nth-child(2)")
+// seconditem.style.backgroundColor="green"
+
+
+// var thirditem=document.querySelector(".list-group-item:nth-child(3)")
+// thirditem.style.color="white"
+
+
+
+//QuerySlectorAll
+var titles = document.querySelectorAll('.title')
+console.log(titles)
+
+var odd=document.querySelectorAll("li:nth-child(odd)")
+
+for (var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green'
 }
+var li=document.querySelectorAll("li")
+console.log(li[1])
+li[1].style.color="green"
