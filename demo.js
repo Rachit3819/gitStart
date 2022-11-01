1215f87e4007b6fe2fcb44e4280ddd812bcab79b
Rachit3819 <rachitagarwal2016@gmail.com>
2022-11-02 00:40:47 +0530

    domtask6

--- demo.js
+++ demo.js
@@ -1,1 +1,28 @@
-console.log("i love git")
+QuerySlector
+var header = document.querySelector ('#main-header')
+header.style.borderBottom ='solid 3px #ccc'
+
+var input = document.querySelector ('input')
+input.value='HelloWorld'
+
+var seconditem=document.querySelector(".list-group-item:nth-child(2)")
+seconditem.style.backgroundColor="green"
+
+
+var thirditem=document.querySelector(".list-group-item:nth-child(3)")
+thirditem.style.color="white"
+
+
+
+//QuerySlectorAll
+var titles = document.querySelectorAll('.title')
+console.log(titles)
+
+var odd=document.querySelectorAll("li:nth-child(odd)")
+
+for (var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green'
+}
+var li=document.querySelectorAll("li")
+console.log(li[1])
+li[1].style.color="green"
