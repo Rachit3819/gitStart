f05ad7dcaedb73887643c87d509f6da4ed4bab7a
Rachit3819 <rachitagarwal2016@gmail.com>
2022-11-01 14:28:47 +0530

    domtask

---
+++ domTask3.js
@@ -0,0 +1,28 @@
+//console.dir(document)
+//console.log(document.domain)
+//console.log(document.URL)
+//console.log(document.all[10])
+//document.all[10].textContent="hello"
+//console.log(document.links)
+//console.log(document.images)
+//console.log(document.forms)
+
+
+//Get Element By ID 
+
+//console.log(document.getElementById("header-title"))
+//var headerTitle=document.getElementById('header-title')
+//var header=document.getElementById('main-header')
+
+//console.log(headerTitle)
+//headerTitle.textContent="HelloWorld"
+//headerTitle.innerHTML="JavaScript"
+//console.log(headerTitle.innerText)
+//console.log(headerTitle.innerText)
+//headerTitle.innerHTML='<h3>HelloWorld</h3>'
+
+//header.style.borderBottom="solid 3px #000"
+
+console.log(document.getElementById('items'))
+items.style.fontWeight="bold"
+items.style.color="green"
