ed1ab77819490a92d7d2571d3594621db9cbab84
Rachit3819 <rachitagarwal2016@gmail.com>
2022-11-06 02:34:54 +0530

    domTask10Bookingappoinment

---
+++ domtask10bookingAppoinment.js
@@ -0,0 +1,11 @@
+// function saveToLocalStorage(event){
+//     event.preventDefault();
+//     const name=event.target.UserName.value
+//     const email=event.target.emailID.value
+//     const phonenumber=event.target.Phonenumbe.value
+//     localStorage.setItem('name',name)
+//     localStorage.setItem('email',email)
+//     localStorage.setItem('phonenumber',phonenumber)
+
+
+// }
